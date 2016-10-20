b9c60d80e9148e34cc3e4b1c05f58739da213325
Yuki Yoshida <yi-yoshida@kddi.com>
2016-10-20 11:39:34 +0900

    kaっていうかきくけこを表示するfunctionを作成

--- echo.php
+++ echo.php
@@ -6,10 +6,15 @@ function execute() {
 
     $return = "";
     $return .= a();
+    $return .= ka();
 
     return "$return\n";
 }
 
 function a() {
   return "あいうえお\n";
+}
+
+function ka() {
+  return "かきくけこ";
 }
